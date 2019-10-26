b31cda56754540515447976c6c83bf4381aa4d6d
Christian Ducrot <christian@ducrot.de>
2019-10-26 16:31:34 +0200

    Improve asset task

--- tasks/assets.js
+++ tasks/assets.js
@@ -5,6 +5,8 @@
 import gulp from 'gulp'
 import plumber from 'gulp-plumber'
 import imagemin from 'gulp-imagemin'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminPngquant from 'imagemin-pngquant'
 
 import config from './config'
 import { onError } from './misc'
@@ -13,7 +15,26 @@ import { onError } from './misc'
 function assets() {
     return gulp.src(config.assets.src)
         .pipe(plumber({errorHandler: onError}))
-        .pipe(imagemin())
+        .pipe(imagemin([
+            // Lossless:
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: false},
+                    {removeDimensions: true}
+                ]
+            }),
+            // Lossy compression
+            imageminMozjpeg({
+                quality: 85,
+                progressive: true
+            }),
+            imageminPngquant({
+                quality: [0.8, 0.85]
+            })
+        ]))
         .pipe(gulp.dest(config.assets.dest));
 }
 
