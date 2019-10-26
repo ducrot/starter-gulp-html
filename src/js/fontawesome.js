91e12c9ec2404aa284a7522ba7406e1cd94b9ae7
Christian Ducrot <christian@ducrot.de>
2019-10-26 17:23:33 +0200

    Add Font Awesome 5 (PRO)

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -2,23 +2,24 @@
  * Font Awesome
  * SVG JavaScript Core
  * https://fontawesome.com/how-to-use/on-the-web/advanced/svg-javascript-core
+ * https://fontawesome.com/how-to-use/with-the-api/setup/importing-icons
  * https://fontawesome.com/icons?d=gallery
  */
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
 
-// fas
+// fas free-solid-svg-icons
 import {
-    faHome, faBars, faCheck
+    faHome, faBars, faCheck, faCircle
 } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
-    faHome, faBars, faCheck
+    faHome, faBars, faCheck, faCircle
 )
 
 
-// far
+// far free-regular-svg-icons
 import {
     faSmile
 } from '@fortawesome/free-regular-svg-icons'
@@ -26,12 +27,43 @@ import {
 library.add(faSmile)
 
 
-// fab
+// fab free-brands-svg-icons
 import {
-    faApple
+    faApple,
+    faYoutube, faFacebookF, faTwitter, faXing, faPinterest, faInstagram
 } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faApple)
+library.add(faApple, faYoutube, faFacebookF, faTwitter, faXing, faPinterest, faInstagram)
+
+
+// fas pro-solid-svg-icons
+import {
+    faLightbulbOn as fasFaLightbulbOn
+} from '@fortawesome/pro-solid-svg-icons'
+
+library.add(
+    fasFaLightbulbOn
+)
+
+
+// far pro-regular-svg-icons
+import {
+    faLightbulbOn as farFaLightbulbOn, faSearch as farFaSearch
+} from '@fortawesome/pro-regular-svg-icons'
+
+library.add(
+    farFaLightbulbOn, farFaSearch
+)
+
+
+// fal pro-light-svg-icons
+import {
+    faCircle as falFaCircle
+} from '@fortawesome/pro-light-svg-icons'
+
+library.add(
+    falFaCircle
+)
 
 
 // Kicks off the process of finding <i> tags and replacing with <svg>
